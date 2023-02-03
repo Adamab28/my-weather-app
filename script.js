9467acabe8ad49b8133ba0439d9669530bba7d55
Adamab28 <124196478+Adamab28@users.noreply.github.com>
2023-02-03 17:22:18 +0000

    Update script.js

--- script.js
+++ script.js
@@ -38,7 +38,6 @@ currentDate.innerHTML = `${days[day]} ${date} ${months[month]}, ${hours}:${minut
 function searchCityname(cityName) {
   let apiKey = "1d038ee28ef2727a9f0310860ac10ae9";
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&apid=${apiKey}&units=metric`;
-
   axios.get(apiUrl).then(displayTemperature);
 }
 
